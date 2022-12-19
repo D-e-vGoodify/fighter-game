35f854ae686d1fb3271f7ce428b7f76efe2102bc
D-e-vGoodify <116831140+D-e-vGoodify@users.noreply.github.com>
2022-12-19 11:25:13 +0100

    Update index.js

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 570
+canvas.height = 320
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
